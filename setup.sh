96a779ef1d12e3f943fe04d9a52a0b2defe8c422
Lucas Bataglião <lucas@batagliao.com>
2019-11-17 18:59:10 -0300

    updated homebrew cask tapping

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ if test ! $(which brew); then
   echo "Installing Homebrew ..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo "Installing Cask"
-  brew tap caskroom/cask
+  brew tap homebrew/cask-cask
 else
   echo "Updating Homebrew ..."
   brew update
